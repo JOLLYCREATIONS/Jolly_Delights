aa43d61c9b849112f6ad984cc9899247cb41af4e
test1 <test@1>
2024-02-06 13:37:14 +0530

    m

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,19 @@
     <div class="flex flex-col w-full items-center space-y-12">
         <div class="relative w-full h-screen">
             <video src="/vid.mp4" muted autoplay :controls="false" loop
-                class="w-full h-screen object-cover blur-sm absolute z-10"></video>
-            <div class="relative z-20 text-2xl text-white">Hello Hello</div>
+                class="w-full h-screen object-cover  absolute z-10"></video>
+            <div class="relative z-20 text-2xl text-white">Want to taste something Delicious</div>
         </div>
         <div class="p-4 rounded-xl bg-[#CBE7D8] text-white w-full">Popular Items</div>
         <div class="p-4 rounded-xl bg-[#fffff] text-[#206038] font-bold text-2xl w-full">Try Our New Arrivals</div>
         <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-4">
-            <MiscBox v-for = "food in foods" :key="food.name" :name="food.name" :image="food.image" />
+            <MiscBox v-for="food in foods" :key="food.name" :name="food.name" :image="food.image" />
         </div>
     </div>
 </template>
 <script setup lang="ts">
-    const foods = [
-  {name: "Dosa", image: "/dosa.png"},
-  {name: "Idly", image: "/idly.png"},
+const foods = [
+    { name: "Dosa", image: "/dosa.jpg" },
+    { name: "Idly", image: "/idli.jpg" },
 ]
 </script>
