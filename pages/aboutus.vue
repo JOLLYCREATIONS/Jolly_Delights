384b990f0bbe121c4253bd7bea239be9f2f13d33
Matheshwaran <matheshwa8@gmail.com>
2024-02-12 19:20:37 +0530

    updated about us page

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="relative w-full h-screen flex flex-col justify-center">
-        <img src="/header.webp" class="w-full h-screen object-cover  absolute z-10">
+    <div class="relative w-full h-[50vh] flex flex-col justify-center">
+        <img src="/header.webp" class="w-full h-[50vh] object-cover  absolute z-10 top-0">
+    </div>
+    <div class="p-4 mt-8 rounded-xl  bg-[#CBE7D8] text-black text-2xl grid grid-cols-1 lg:grid-cols-2">
+        <h1 class="text-center mt-2 font-bold">Our Story</h1>
+        <p class="mt-4 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur finibus tempor. Quisque
+            porta commodo sodales. Cras cursus vel dui non lacinia. Vestibulum ut vulputate dolor. Orci varius natoque penatibus et magnis
+            dis parturient montes, nascetur ridiculus mus. Maecenas felis augue, placerat in erat sit amet, malesuada
+            venenatis odio. Sed euismod luctus ornare. Maecenas mollis in lectus id ornare. Nunc quis eleifend elit, sed
+            mattis nulla. Suspendisse malesuada neque vitae nulla rhoncus pharetra. Praesent auctor condimentum vestibulum.
+        </p>
+        <img src="/idli.jpg" class="p-8">
+    </div>
+    <div class="p-4 mt-8 rounded-xl  bg-[#CBE7D8] text-black text-2xl grid grid-cols-1 lg:grid-cols-2">
+        <h1 class="text-center mt-2 font-bold">Our Story</h1>
+        <p class="mt-4 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur finibus tempor. Quisque
+            porta commodo sodales. Cras cursus vel dui non lacinia. Vestibulum ut vulputate dolor. Orci varius natoque penatibus et magnis
+            dis parturient montes, nascetur ridiculus mus. Maecenas felis augue, placerat in erat sit amet, malesuada
+            venenatis odio. Sed euismod luctus ornare. Maecenas mollis in lectus id ornare. Nunc quis eleifend elit, sed
+            mattis nulla. Suspendisse malesuada neque vitae nulla rhoncus pharetra. Praesent auctor condimentum vestibulum.
+        </p>
+        <img src="/idli.jpg" class="p-8">
     </div>
 </template>
