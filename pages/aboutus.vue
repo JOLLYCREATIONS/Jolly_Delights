974ec562b3009dcb8c86f3de5a9bf38e231fee6f
Matheshwaran <matheshwa8@gmail.com>
2024-02-12 20:01:32 +0530

    alignment

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -3,7 +3,7 @@
         <img src="/header.webp" class="w-full h-[50vh] object-cover  absolute z-10 top-0">
     </div>
     <div class="p-4 mt-8 rounded-xl  bg-[#CBE7D8] text-black text-2xl grid grid-cols-1 lg:grid-cols-2">
-        <h1 class="text-center mt-2 font-bold">Our Story</h1>
+        <h1 class="text-center mt-2 align-top font-bold">Our Story</h1>
         <p class="mt-4 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur finibus tempor. Quisque
             porta commodo sodales. Cras cursus vel dui non lacinia. Vestibulum ut vulputate dolor. Orci varius natoque penatibus et magnis
             dis parturient montes, nascetur ridiculus mus. Maecenas felis augue, placerat in erat sit amet, malesuada
@@ -14,12 +14,12 @@
     </div>
     <div class="p-4 mt-8 rounded-xl  bg-[#CBE7D8] text-black text-2xl grid grid-cols-1 lg:grid-cols-2">
         <h1 class="text-center mt-2 font-bold">Our Story</h1>
+        <img src="/idli.jpg" class="p-8">
         <p class="mt-4 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur finibus tempor. Quisque
             porta commodo sodales. Cras cursus vel dui non lacinia. Vestibulum ut vulputate dolor. Orci varius natoque penatibus et magnis
             dis parturient montes, nascetur ridiculus mus. Maecenas felis augue, placerat in erat sit amet, malesuada
             venenatis odio. Sed euismod luctus ornare. Maecenas mollis in lectus id ornare. Nunc quis eleifend elit, sed
             mattis nulla. Suspendisse malesuada neque vitae nulla rhoncus pharetra. Praesent auctor condimentum vestibulum.
         </p>
-        <img src="/idli.jpg" class="p-8">
     </div>
 </template>
