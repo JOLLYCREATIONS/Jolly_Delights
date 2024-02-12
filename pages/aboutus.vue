49afcc7e00c4147c59b9afae94944a87cb4b3369
JerryChopsuey <varunprakashchinnadurai@gmail.com>
2024-02-12 20:15:41 +0530

    aboutusupdated

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -22,4 +22,14 @@
             mattis nulla. Suspendisse malesuada neque vitae nulla rhoncus pharetra. Praesent auctor condimentum vestibulum.
         </p>
     </div>
+    <div class="p-4 mt-8 rounded-xl  bg-[#CBE7D8] text-black text-2xl grid grid-cols-1 lg:grid-cols-2">
+        <h1 class="text-center mt-2 font-bold">Our Story</h1>
+        <img src="/idli.jpg" class="p-8">
+        <p class="mt-4 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur finibus tempor. Quisque
+            porta commodo sodales. Cras cursus vel dui non lacinia. Vestibulum ut vulputate dolor. Orci varius natoque penatibus et magnis
+            dis parturient montes, nascetur ridiculus mus. Maecenas felis augue, placerat in erat sit amet, malesuada
+            venenatis odio. Sed euismod luctus ornare. Maecenas mollis in lectus id ornare. Nunc quis eleifend elit, sed
+            mattis nulla. Suspendisse malesuada neque vitae nulla rhoncus pharetra. Praesent auctor condimentum vestibulum.
+        </p>
+    </div>
 </template>
