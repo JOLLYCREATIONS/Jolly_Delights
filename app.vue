1a7a70ad1477942bdc48e48821a757dfee45406d
Mathesh <matheshwa8@gmail.com>
2024-02-15 15:42:09 +0530

    update

--- app.vue
+++ app.vue
@@ -9,16 +9,6 @@
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300;400;500;600;700&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap");
 
-@font-face {
-  font-family: Atmospheric;
-  src: url(/atmospheric.ttf);
-}
-
-@font-face {
-  font-family: Azonix;
-  src: url(/azonix.otf);
-}
-
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.5s;
@@ -32,5 +22,9 @@
 .page-leave-active {
   opacity: 0;
   transition-duration: 420ms;
+}
+
+.font-montserrat {
+  font-family: 'Montserrat', 'Arial', sans-serif;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen flex-col w-full tracking-wide">
+    <div class="min-h-screen flex-col w-full tracking-wide font-montserrat">
         <LayoutHeader class="z-40" />
         <main class="grow-1 w-full relative z-20">
             <slot></slot>
