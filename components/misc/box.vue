4adfb47fe564dd8d552faa4fc844f56e5b3b93e4
Matheshwaran Prakash <matheshwa8@gmail.com>
2024-03-14 14:59:22 +0530

    add btn

--- components/misc/box.vue
+++ components/misc/box.vue
@@ -1,8 +1,13 @@
 <template>
+
     <div><img class="rounded-xl w-full lg:w-60 md:w-48 object-cover" :src="image" />
         <div class="md:text-lg lg:text-xl font-semibold ">{{ name }}</div>
+        <div class="md:text-lg lg:text-base truncate3  font-semibold ">{{ name }}</div>
+        <button class="bg-green-800 block mt-2 p-2 w-full mx-auto rounded-md text-white"
+            @click="() => emit(`added`)">Add To Cart</button>
     </div>
 </template>
 <script setup lang="ts">
 const { name, image } = defineProps<{ name: string, image: string }>()
+const emit = defineEmits<{ (e: "added"): void }>()
 </script>
