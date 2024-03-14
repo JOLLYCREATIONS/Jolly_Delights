c57cd532ee98f47d43ecb153c47af4cd27f7eb11
Matheshwaran Prakash <matheshwa8@gmail.com>
2024-03-14 15:00:36 +0530

    fix boxes

--- components/misc/box.vue
+++ components/misc/box.vue
@@ -1,7 +1,6 @@
 <template>
 
     <div><img class="rounded-xl w-full lg:w-60 md:w-48 object-cover" :src="image" />
-        <div class="md:text-lg lg:text-xl font-semibold ">{{ name }}</div>
         <div class="md:text-lg lg:text-base truncate3  font-semibold ">{{ name }}</div>
         <button class="bg-green-800 block mt-2 p-2 w-full mx-auto rounded-md text-white"
             @click="() => emit(`added`)">Add To Cart</button>
--- components/misc/circle.vue
+++ components/misc/circle.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="flex flex-col items-center gap-3"><img class="rounded-full w-full xl:w-45 h-45 lg:w-32  lg:h-32 md:w-32 md:h-32" :src="image" />
+    <div class="flex flex-col items-center gap-3"><img
+            class="rounded-full w-full xl:w-45 h-45 lg:w-28  lg:h-28 md:w-32 md:h-32" :src="image" />
         <div class="md:text-lg lg:text-xl text-center font-semibold ">{{ name }}</div>
     </div>
 </template>
