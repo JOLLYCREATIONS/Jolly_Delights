3abb7a6ab7ff8489b34ee85381f01682f6cf03e2
Matheshwaran <matheshwa8@gmail.com>
2024-03-15 09:47:15 +0530

    added hover effects

--- components/misc/box.vue
+++ components/misc/box.vue
@@ -2,7 +2,7 @@
 
     <div><img class="rounded-xl w-full lg:w-60 md:w-48 object-cover" :src="image" />
         <div class="md:text-lg lg:text-base truncate3  font-semibold ">{{ name }}</div>
-        <button class="bg-green-800 block mt-2 p-2 w-full mx-auto rounded-md text-white"
+        <button class="bg-green-800 transition ease-in-out hover:bg-yellow-600 block mt-2 p-2 w-full mx-auto rounded-md text-white"
             @click="() => emit(`added`)">Add To Cart</button>
     </div>
 </template>
