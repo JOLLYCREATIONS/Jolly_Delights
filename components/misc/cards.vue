dcfdb2af56adee719493ff986965c671caaea305
NeTT <nett@nett.moe>
2024-03-15 10:34:05 +0530

    fix card transition

--- components/misc/cards.vue
+++ components/misc/cards.vue
@@ -1,25 +1,34 @@
 <template>
     <div class="flex items-center h-[25rem] relative max-w-7xl w-full mx-auto justify-center overflow-hidden"
         :style="{ perspective: `100rem`, }">
-        <div v-for="item, i in items" :key="`${item.name}_${i}`"
-            :class="`transition-all duration-1000 ease-in-out absolute top-0 mx-auto w-24 lg:w-60 md:w-48 inset-x-0`"
-            :style="{ marginTop: `3rem`, transform: `rotateY(${-10 * (i - current)}deg) translateX(${(Math.abs(i - current) >= 3) ? 1 : ((i - current) * 5)}rem) translateZ(${-1 * ((Math.abs(i - current) >= 3) ? 1 : Math.abs(i - current) * 15)}rem)`, transformStyle: `preserve-3d`, zIndex: -Math.abs(i - current) * 10, opacity: (Math.abs(i - current) < 3) ? 1 : 0 }">
+        <div v-for="item, i in data" :key="`${item.name}_${item.i}`"
+            :class="`transition-all duration-500 ease-in-out absolute top-0 mx-auto w-24 lg:w-60 md:w-48 inset-x-0`"
+            :style="{ marginTop: `3rem`, transform: `rotateY(${-10 * (item.i - current)}deg) translateX(${(Math.abs(item.i - current) >= 3) ? 1 : ((item.i - current) * 8)}rem) translateZ(${-1 * ((Math.abs(item.i - current) >= 3) ? 1 : Math.abs(item.i - current) * 15)}rem)`, transformStyle: `preserve-3d`, zIndex: -Math.abs(item.i - current) * 10, opacity: (Math.abs(item.i - current) < 3) ? 1 : 0 }">
             <img class="rounded-xl w-24 h-24 lg:w-60 md:w-48 lg:h-60 md:h-48 shadow-xl transition-all duration-500 ease-in-out"
                 :src="item.image" />
         </div>
         <div :class="`md:text-lg lg:text-xl font-semibold relative top-[7rem]`">{{
-            items[current].name }}
+            data[at].name }}
         </div>
     </div>
 </template>
 <script setup lang="ts">
 const { items } = defineProps<{ items: { name: string, image: string }[] }>()
 
+const data = ref(items.map((x, i) => ({ ...x, i })))
+
+const mult = 360 / items.length
+
 const current = ref(2)
+
+const at = computed(() => current.value % items.length)
 
 onMounted(() => {
     setInterval(() => {
-        current.value = current.value === items.length - 3 ? 2 : current.value + 1
+        current.value += 1
+        setTimeout(() => {
+            data.value = [...data.value.slice(1), { name: data.value[0].name, image: data.value[0].image, i: (data.value.at(-1)?.i || 0) + 1 }]
+        }, 500)
     }, 3000)
 })
 
