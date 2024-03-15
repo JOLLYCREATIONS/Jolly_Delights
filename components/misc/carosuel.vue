a3a5143c161fb1e344ed6644172182823726b912
NeTT <nett@nett.moe>
2024-03-15 10:33:56 +0530

    remove buttons

--- components/misc/carosuel.vue
+++ components/misc/carosuel.vue
@@ -10,27 +10,6 @@
                     <div class="text-sm">{{ item.review }}</div>
                 </div>
             </div>
-            <button class="absolute left-0 inset-y-0 my-auto stroke-white fill-white stroke-2 transform rotate-180"
-                @click="prev">
-                <div class="rounded-full p-2 bg-black/70">
-                    <svg width="24" height="24" viewBox="0 0 48 48" fill="white" stroke="white"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M24.2561 32.3144L23.5976 33.067C24.325 33.7035 25.4306 33.6298 26.067 32.9024L33.067 24.9024C33.6443 24.2426 33.6443 23.2574 33.067 22.5976L26.067 14.5976C25.4306 13.8703 24.325 13.7966 23.5976 14.433C22.8703 15.0694 22.7965 16.175 23.433 16.9024L27.8934 22H15.75C14.7835 22 14 22.7835 14 23.75C14 24.7165 14.7835 25.5 15.75 25.5H27.8934L23.433 30.5976C22.7965 31.325 22.8703 32.4306 23.5976 33.067L24.2561 32.3144Z"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </div>
-            </button>
-            <button class="absolute right-0 inset-y-0 my-auto stroke-white fill-white stroke-2" @click="next">
-                <div class="rounded-full p-2 bg-black/70">
-                    <svg width="24" height="24" viewBox="0 0 48 48" fill="white" stroke="white"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M24.2561 32.3144L23.5976 33.067C24.325 33.7035 25.4306 33.6298 26.067 32.9024L33.067 24.9024C33.6443 24.2426 33.6443 23.2574 33.067 22.5976L26.067 14.5976C25.4306 13.8703 24.325 13.7966 23.5976 14.433C22.8703 15.0694 22.7965 16.175 23.433 16.9024L27.8934 22H15.75C14.7835 22 14 22.7835 14 23.75C14 24.7165 14.7835 25.5 15.75 25.5H27.8934L23.433 30.5976C22.7965 31.325 22.8703 32.4306 23.5976 33.067L24.2561 32.3144Z"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </div>
-            </button>
         </div>
         <div class="flex items-center gap-2">
             <button v-for="item, i in images" :key="item" :class="`w-8 h-8 rounded-full ${Math.abs(current - i)<=1 ? `bg-yellow-600` : `bg-green-900`}`" @click="() => current = i"></button>
