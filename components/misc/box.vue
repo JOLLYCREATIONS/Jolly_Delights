9a11533c9a05196f8560e6325b5c4d28b9670e11
Matheshwaran <matheshwa8@gmail.com>
2024-03-28 16:33:22 +0530

    trying lap size

--- components/misc/box.vue
+++ components/misc/box.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center md:w-48 sm:w-36 w-24 ">
-        <div class="rounded-xl md:w-40 md:h-40 sm:w-32 sm:h-32 w-24 h-24 xl:w-40 xl:h-40">
-        <img class="rounded-xl md:w-40 md:h-40 sm:w-32 sm:h-32 w-24 h-24 object-cover transition duration-500 ease-in-out transform hover:scale-125" :src="image" />
+        <div class="rounded-xl md:w-40 md:h-40 sm:w-32 sm:h-32  lg:w-20 lg:h-20 h-24 w-24 xl:w-40 xl:h-40">
+        <img class="rounded-xl md:w-40 md:h-40 sm:w-32 sm:h-32 w-24 h-24 lg:w-20 lg:h-20 object-cover transition duration-500 ease-in-out transform hover:scale-125" :src="image" />
     </div>
         <div class="md:text-lg mt-4 lg:text-base truncate font-semibold ">{{ name }}</div>
         <button
